76a1c3257660f8cec713124b4d6919104cd3406d
johshisha <johshisha@gmail.com>
2019-01-27 03:31:01 +0900

    Set channel of slack to post messages from environment variable

--- app/models/webhooks/to/slack.rb
+++ app/models/webhooks/to/slack.rb
@@ -1,7 +1,7 @@
 class Webhooks::To::Slack
   def initialize(mention:, url:, additional_message:)
     @mention = "@#{mention}"
-    @channel = @mention == '@everyone' ? '#general' : @mention
+    @channel = ENV.fetch('SLACK_CHANNEL') || @mention
     @text = "#{@mention} #{url} #{additional_message}"
     @webhook_uri = ENV.fetch('SLACK_WEBHOOK_URL')
   end
