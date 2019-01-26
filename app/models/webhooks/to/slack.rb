fd468e43e1f8131992138368bb17ddd9f76bf060
johshisha <johshisha@gmail.com>
2019-01-27 03:34:58 +0900

    Push notification via mention in slack by username

--- app/models/webhooks/to/slack.rb
+++ app/models/webhooks/to/slack.rb
@@ -2,11 +2,14 @@ class Webhooks::To::Slack
   def initialize(mention:, url:, additional_message:)
     @mention = "@#{mention}"
     @channel = ENV.fetch('SLACK_CHANNEL') || @mention
-    @text = "#{@mention} #{url} #{additional_message}"
+    @text = "<#{@mention}> #{url} #{additional_message}"
     @webhook_uri = ENV.fetch('SLACK_WEBHOOK_URL')
   end
 
   def post
+    payload_json = { payload: {text: @text, link_names: 1, channel: @channel}.to_json
+    # FIXME: to_jsonで<>がescapeされてほしくないので，とりあえず無理やり修正している
+    payload = payload_json.gsub(/\\u003c|\\u003e/, "\\u003c" => "<", "\\u003e" => ">")
     HttpPostJob.perform_later(@webhook_uri, { payload: {text: @text, link_names: 1, channel: @channel}.to_json })
   end
 end
